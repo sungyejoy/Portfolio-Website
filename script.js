6930bc019fd238efba1e7090789029ab3edee4db
yeji <sungyejoy@gmail.com>
2025-03-30 14:13:27 +0800

    why does the draggable carousell not work..

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-new Swiper('.card-wrapper', {
-    loop: true,
+const carousel = document.querySelector(".carousel");
 
-    // Pagination bullets
-    pagination: {
-    el: '.swiper-pagination',
-    },
+let isDragging = false, startX, startScrollLeft;
 
-    // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
-});
+const dragStart = () => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+
+    //records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; //if isdragging is false return from here
+    // updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
